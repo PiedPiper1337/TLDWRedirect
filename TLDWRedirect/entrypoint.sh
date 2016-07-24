9c257255d932882b4c25ed0fc233e64c950caf4d
brianzhao <bzspartan@gmail.com>
2016-07-24 13:40:31 -0700

    containerized with Docker

---
+++ TLDWRedirect/entrypoint.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+/root/tldwredirect-1.0-SNAPSHOT/bin/tldwredirect -Dhttp.port=disabled -Dhttps.port=443 -Dhttps.keyStore=/root/secrets/sumyoutube.jks -Dhttps.keyStorePassword=123456
